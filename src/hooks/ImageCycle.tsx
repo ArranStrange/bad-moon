c72a73cf6bd7c79888c89bac7051ad4b5bcba6a9
Arran <arranstrange@googlemail.com>
2024-03-01 17:06:56 +0000

    small mobile updates

--- src/hooks/ImageCycle.tsx
+++ src/hooks/ImageCycle.tsx
@@ -17,23 +17,10 @@ const ImageCycle: React.FC<{ images: string[] }> = ({ images }) => {
     return () => window.clearInterval(intervalRef.current);
   }, [images]);
 
-  const handleMouseEnter = () => {
-    window.clearInterval(intervalRef.current);
-  };
-
-  const handleMouseLeave = () => {
-    window.clearInterval(intervalRef.current);
-    intervalRef.current = window.setInterval(() => {
-      setCurrentIndex((prevIndex) => {
-        const newIndex = prevIndex + 1;
-        return newIndex >= images.length ? 0 : newIndex;
-      });
-    }, 1000);
-  };
-
   return (
     <div>
       {images.map((image, index) => (
+        // eslint-disable-next-line jsx-a11y/img-redundant-alt
         <img
           key={index}
           src={image}
@@ -42,8 +29,6 @@ const ImageCycle: React.FC<{ images: string[] }> = ({ images }) => {
             display: index === currentIndex ? "block" : "none",
             height: "100%",
           }}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
         />
       ))}
     </div>
